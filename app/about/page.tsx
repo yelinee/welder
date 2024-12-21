bea5ac73934dbf3d06ce8978450e49712ff11e6f
yelinee <ami573@naver.com>
2024-12-21 09:25:23 +0900

    feat: about 페이지 추가

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+    return (
+        <div className="text-center mt-24">
+            <h1 className="text-5xl font-bold p-3">Welder Team</h1>
+            <p className="text-xl p-3">웰더팀의 비전</p>
+        </div>
+)
+    ;
+}
